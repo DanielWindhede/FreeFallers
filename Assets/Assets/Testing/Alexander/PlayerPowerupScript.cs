910fc273c71fd64387cdfa3e852eb3d10bf652c9
Alexander Macri <alexander.m.1999@gmail.com>
2020-06-24 23:11:37 +0200

    La in lite skit och börja på down dash

--- Assets/Assets/Testing/Alexander/PlayerPowerupScript.cs
+++ Assets/Assets/Testing/Alexander/PlayerPowerupScript.cs
@@ -1,12 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class PlayerPowerupScript : MonoBehaviour
 {
     public PowerupScript.PowerupType currentPowerup;
-
+    
     private PlayerInput _playerInput;
+
+    private Rigidbody _playerRB;
+
+    [SerializeField]float _dashSpeed;
+    [SerializeField] float _dashTime;
+
+    public StateMachine<PlayerPowerupScript> powerupStateMachine;
+    
+    public DefaultState defaultState = new DefaultState();
+    //public DownDashState downDashState = new DownDashState();
+
+    
+    
 
     private void OnEnable()
     {
@@ -28,6 +42,13 @@ public class PlayerPowerupScript : MonoBehaviour
             _playerInput = new PlayerInput();
 
         _playerInput.PlayerControls.UsePowerup.performed += ctx => UsePowerup();
+
+        _playerRB = GetComponent<Rigidbody>();
+
+        powerupStateMachine = new StateMachine<PlayerPowerupScript>(this);
+        defaultState = new DefaultState();
+
+        powerupStateMachine.ChangeState(defaultState);
     }
     void Start()
     {
@@ -37,6 +58,8 @@ public class PlayerPowerupScript : MonoBehaviour
     void Update()
     {
         //print(GetComponent<Rigidbody>().velocity.y);
+
+        powerupStateMachine.Update();
     }
 
     private void UsePowerup()
@@ -49,13 +72,75 @@ public class PlayerPowerupScript : MonoBehaviour
                 print("No power");
                 break;
             case 1:
+                //kolla att man är i luften 
                 print("Used nr 1");
+                powerupStateMachine.ChangeState(new DownDashState(_playerRB, _dashSpeed, _dashTime));
                 break;
             case 2:
                 print("Used nr 2");
                 break;
         }
 
+        //flytta sen
         currentPowerup = PowerupScript.PowerupType.None;
     }
 }
+
+public class DefaultState : State<PlayerPowerupScript>
+{
+    public DefaultState()
+    {
+
+    }
+
+    public override void EnterState(PlayerPowerupScript owner)
+    {
+
+    }
+
+    public override void ExitState(PlayerPowerupScript owner)
+    {
+
+    }
+
+    public override void UpdateState(PlayerPowerupScript owner)
+    {
+
+    }
+}
+
+public class DownDashState : State<PlayerPowerupScript>
+{
+    Rigidbody _playerRB;
+    float _dashSpeed;
+    float _dashTime;
+    Timer _timer;
+
+    public DownDashState(Rigidbody playerRB, float dashSpeed, float dashTime)
+    {
+        _playerRB = playerRB;
+        _dashSpeed = dashSpeed;
+        _dashTime = dashTime;
+    }
+
+    public override void EnterState(PlayerPowerupScript owner)
+    {
+        _timer = new Timer(_dashTime);
+    }
+
+    public override void ExitState(PlayerPowerupScript owner)
+    {
+        //owner.currentPowerup = PowerupScript.PowerupType.None;
+    }
+
+    public override void UpdateState(PlayerPowerupScript owner)
+    {
+        _playerRB.velocity = new Vector3(_playerRB.velocity.x, -_dashSpeed, 0f);
+        Debug.Log("GOOOO");
+        if (_timer.Expired)
+        {
+            owner.powerupStateMachine.ChangeState(owner.defaultState);
+        }
+    }
+}
+
